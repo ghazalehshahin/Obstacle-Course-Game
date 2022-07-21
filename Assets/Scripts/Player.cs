c8f682f2324eb233de5fb6304a6a4682a338fef0
ghazalehshahin <shahinghazaleh@gmail.com>
2022-07-21 20:04:11 +0430

    bugs fixed. score limit added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
-{    
-    Scorer scorer = new Scorer();
+{  
+    //attributes and methods (position)  
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float rotateSpeed = 200f;
+    public void getData()
+    {
+        Debug.Log("moveSpeed:" + moveSpeed + "**rotateSpeed:" + rotateSpeed);
+    }
+     public void setData(float mspeed, float rspeed)
+    {
+        moveSpeed = mspeed;
+        rotateSpeed = rspeed;
+    }
+
+
+    //attributes and methods (score) 
+
+    private int score = 10;
+    public int getScore()
+    {
+        return score;
+    }
+    public void setScore(int sc)
+    {
+        score = sc;
+    }
+   
+
+    //methods
     void moveGiz()
     {
         float xVal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
@@ -14,29 +38,28 @@ public class Player : MonoBehaviour
         float yVal = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
         transform.Translate(xVal, 0 , zVal);
         transform.Rotate(0, yVal, 0);
-        scorerHandler();
-    }
-    
-    private void OnCollisionEnter(Collision other)
-    {
-        scorer.collisionHandler(other);
-    }
-
-    void scorerHandler()
-    {
-        if(scorer.score == 0)
+        if(getScore() == 0)
         {
-            moveSpeed = 0f;
-            rotateSpeed = 0f;
-            Debug.Log("You lost honey :(");
+            setData(0f,0f);
         }
     }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag != "Hit")
+        {
+            int sc = getScore();
+            sc --;
+            setScore(sc);
+            Debug.Log("your is score is:" + getScore());
+        }
+    }
+    
+
+    //start and update methods
     void Start()
     {
         Debug.Log("Start the game Giz :)");
     }
-
-    // Update is called once per frame
     void Update()
     {
         moveGiz();
